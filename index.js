3aa575ea6c299b3e93391424e6e30e8fa80f7cea
Obi Joyce <obijoyce@gmail.com>
2018-08-04 11:03:40 +0100

    add error handler

--- index.js
+++ index.js
@@ -7,16 +7,27 @@ _.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
 // → { ‘a’: 1, ‘b’: 2, ‘c’: 3 }
 
 // starting http server
-const http = require('http')
-const port = 3000
-const requestHandler = (request, response) => {
-console.log(request.url)
-response.end('Hello Node.js Server!')
-}
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
-if (err) {
-return console.log('something bad happened', err)
-}
-console.log(`server is listening on ${port}`)
+const express = require('express')
+const app = express()
+app.use((request, response, next) => {
+console.log(request.headers)
+next()
 })
+app.use((request, response, next) => {
+request.chance = Math.random()
+next()
+})
+app.get('/', (request, response) => {
+response.json({
+chance: request.chance
+})
+})
+// app.get('/', (request, response) => {
+//     throw new Error('oops')
+//     })
+//  app.use((err, request, response, next) => {
+//         // log the error, for now just console.log
+//       console.log(err)
+//      response.status(500).send('Something broke!')
+//     })
+app.listen(3000)
